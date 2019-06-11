43245100618f6b0087da8947833ee058397de801
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2019-06-11 17:32:05 +0100

    Use ssl-enabled version for deployments

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -2,6 +2,15 @@
 
 set -eux
 
+CERTS=config-storage-clients
+SUBJ="/C=US/ST=Ohio/L=Columbus/O=Acme Company/OU=Acme/CN=ironpad"
+
+openssl genrsa -out $CERTS/server.key 2048
+openssl req -new  -x509 -sha256 -key $CERTS/server.key -out $CERTS/server.crt -days 3650 -subj "$SUBJ"
+
+#openssl req -new -sha256 -key $CERTS/server.key -out $CERTS/server.csr -subj "$SUBJ"
+#openssl x509 -req -sha256 -in $CERTS/server.csr -signkey $CERTS/server.key -out $CERTS/server.crt -days 3650
+
 for worker in worker-debian8 worker-ubuntu16-04; do
   sudo rm -rf "${worker}"
   mkdir -m 0777 "${worker}" "${worker}/build"
